94bafe505552986cc773bc47c21ea0f0b8fdccf6
Marat Shagiev <m_js@bk.ru>
2021-03-28 22:37:47 +0300

    Add Server task. Todo: make download demo file from landing

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,9 +1,9 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 
-const imgMinify = require('./imgMinify');
+const imgMin = require('./imgMin');
 const styles = require('./styles');
 const pug2html = require('./pug2html');
-const scripts = require('./scripts');
+const js = require('./scripts');
 
 const server = require('browser-sync').create();
 
@@ -15,16 +15,14 @@ function readyReload(cb) {
 module.exports = function serve(cb) {
   server.init({
                 server: 'build',
-                notify: false,
+                nitify: false,
                 open: true,
-                cors: true
+                cors: true,
               });
 
-  gulp.watch('src/img/*.{gif,png,jpg,svg,webp}', gulp.series(imgMinify, readyReload));
+  gulp.watch('src/img/*/*.{gif,png,jpg,svg,webp', gulp.series(imgMin, readyReload));
   gulp.watch('src/styles/**/*.scss', gulp.series(styles, cb => gulp.src('build/css').pipe(server.stream()).on('end', cb)));
-  gulp.watch('src/js/**/*.js', gulp.series(scripts, readyReload));
+  gulp.watch('src/js/**/*.js', gulp.series(js, readyReload));
   gulp.watch('src/pages/**/*.pug', gulp.series(pug2html, readyReload));
 
-
-  return cb();
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,16 @@
 const gulp = require('gulp');
 
 const clean = require('./gulp/tasks/clean');
-// const serve = require('./gulp/tasks/serve');
+const serve = require('./gulp/tasks/serve');
 const pug2html = require('./gulp/tasks/pug2html');
 const styles = require('./gulp/tasks/styles');
 const scripts = require('./gulp/tasks/scripts');
 const fonts = require('./gulp/tasks/fonts');
-const imgMinify = require('./gulp/tasks/imgMinify');
+const imgMin = require('./gulp/tasks/imgMin');
 
 const ghPages = require('./gulp/tasks/ghPages');
 
-module.exports.start = gulp.series(clean, pug2html, styles, scripts, fonts, imgMinify);
+module.exports.start = gulp.series(clean, pug2html, styles, scripts, fonts, imgMin, serve);
 
 module.exports.ghPages = gulp.series(ghPages);
 
